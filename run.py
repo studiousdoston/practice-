0af5b7bbb24f75bb2f05a2b571d1e1965abfc5be
DEEN <studiousdoston@gmail.com>
2026-04-15 10:06:41 +0900

    feat: python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+# Dunder (double underscores) __builtins__, __init__
+
+message = "PYTHON: Everything is object!"
+print(message)
+
+result = type(message)
+print("result:", result)
+
+'''we can have multiple lines of comments with tripe single strings
+
+  Built-in tools in Python
+  1. TYPES => int, float, str, list, dict
+  2. FUNCTIONS => print() len() input() type()
+  3. CONSTANTS => True, False, None
+'''
+print(dir(__builtins__))
+
