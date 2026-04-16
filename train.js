28cdee46c2253426fbb507b3b30ceb0b15caa63c
DEEN <studiousdoston@gmail.com>
2026-04-16 16:44:26 +0900

    fix: reformatting the code

---
+++ train.js
@@ -0,0 +1,15 @@
+/*A-TASK (NodeJS)
+
+⭐️Savol:Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi. */
+
+
+//⭐️SOLUTION
+function countLetters(char, str){
+  const splitted = str.split("");
+  const filtered = splitted.filter(ele=>ele===char);
+  return filtered.length
+}
+
+//⭐️RESULT 
+console.log(countLetters("e","engineering"));
