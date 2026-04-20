31f3bcc80c69648dc843f5fb9369186abdf02f8c
DEEN <studiousdoston@gmail.com>
2026-04-20 16:05:54 +0900

    feat: Animal challenge completed q q

--- train.js
+++ train.js
@@ -8,11 +8,12 @@ MASALAN countLetter("e", "engineer") 3ni return qiladi. */
 function countLetters(char, str) {
   const splitted = str.split(""); //array
   const filtered = splitted.filter(ele => ele === char);
-  return filtered.length
+  return filtered.length;
 }
 
 //⭐️RESULT 
 console.log(countLetters("e", "engineering"));
+
 
 
 //*B-TASK (NodeJS)
@@ -22,8 +23,46 @@ console.log(countLetters("e", "engineering"));
 
 //⭐️SOLUTION
 function countNums(str) {
-  return str.split("").map(Number).filter(Boolean).length
+  return str.split("").map(Number).filter(Boolean).length;
 }
 
 //⭐️RESULT
-console.log(countNums("ad2a54y79wet0sfgb9"))
+console.log(countNums("ad2a54y79wet0sfgb9"));
+
+
+
+// CHALLANGE TASK: Animals 
+//⭐️ SAVOL shunday bir funksiya tuzingki unga berilgan argumentdagi harflarni hayvonlar arrayida hayvonlarni harflari bilan solishtirsin va mos kelganlaridan yangi bir array hosil qisin
+
+//⭐️SOLUTION:
+const animals = ["fox", "ant", "bird", "lion", "wolf", "deer", "bear", "frog", "hen", "mole", "duck", "goat", "dog", "cat", "bat", "cow"];
+// STEP 1: seperate the individual array elements into arrays consists of words inside the element --> [ 'w', 'o', 'l', 'f' ],
+const animal = animals.map(ele => ele.split("")); //✅
+
+// STEP 2: seperate the argument's individual letters into one array: "fdgwoalt" --> [ 'f', 'd', 'g', 'w', 'o', 'a','l', 't' ]
+const letter = "fdgwoalt" //✅       // should give us ["goat", "wolf", "dog"]
+const letters = letter.split("");
+
+// STEP 3: compare  the two arrays individual elements, and take the elements which exist in both arrays 
+let array = [];
+for (let i = 0; i < animal.length; i++) {
+  if (animal[i].every(letter => letters.includes(letter))) { array.push(animal[i].join("")) }
+}
+//console.log(array)
+
+// STEP 4: create the function
+function findAnimals(word) {
+  const animal = animals.map(ele => ele.split(""));
+  const letters = word.split("");
+  let arrayOfAnimals = [];
+  for (let i = 0; i < animal.length; i++) {
+    if (animal[i].every(letter => word.includes(letter))) {
+      arrayOfAnimals.push(animal[i].join(""))
+    }
+  }
+  return arrayOfAnimals;
+};   //✅
+const foundAnimals = findAnimals("fdgwoalt");
+
+//⭐️ RESULT:
+console.log(foundAnimals)
