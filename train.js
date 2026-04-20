d79498a73f4438a2f77afb5b32d46dc6a6833633
DEEN <studiousdoston@gmail.com>
2026-04-20 16:54:12 +0900

    fix: revised

--- train.js
+++ train.js
@@ -1,19 +1,3 @@
-/*A-TASK (NodeJS)
-⭐️Savol:Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-MASALAN countLetter("e", "engineer") 3ni return qiladi.
- */
-
-
-// ⭐️ Masalaning yechimi
-function countLetters(char, str) {
-  const splitted = str.split(""); //array
-  const filtered = splitted.filter(ele => ele === char);
-  return filtered.length;
-}
-console.log(countLetters("e", "engineering"));
-
-//====================================================================================
-
 /*B-TASK (NodeJS)
 ⭐️ SAVOL: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
@@ -43,6 +27,25 @@ function findAnimals(word) {
     }
   }
   return arrayOfAnimals;
-};  
+};
 const foundAnimals = findAnimals("fdgwoalt");
 console.log(foundAnimals)
+
+//====================================================================================
+
+// /*A-TASK (NodeJS)
+// ⭐️Savol:Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+//  */
+
+
+// // ⭐️ Masalaning yechimi
+// function countLetters(char, str) {
+//   const splitted = str.split(""); //array
+//   const filtered = splitted.filter(ele => ele === char);
+//   return filtered.length;
+// }
+// console.log(countLetters("e", "engineering"));
+
+
+
