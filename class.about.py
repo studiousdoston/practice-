ad3b9a2e1880a0b1cccd0779b152613c1a006467
DEEN <studiousdoston@gmail.com>
2026-04-20 11:03:40 +0900

    fix: Learn Python magic methods

--- class.about.py
+++ class.about.py
@@ -24,11 +24,11 @@ class Person():
 
     def say_age(self):
         print(f"{self.name} is {self.age} years old.")
-    
-    @classmethod 
-    def explain(s): 
+
+    @classmethod
+    def explain(s):
         print("Static method property executed")
-#NOTE Static methods must have one parameter to be executed correctly. I tried with NO parameter and TWO parameters, both did not work!
+# NOTE Static methods must have one parameter to be executed correctly. I tried with NO parameter and TWO parameters, both did not work!
 
 
 person_obj = Person("Deen", 21)
@@ -38,18 +38,61 @@ person3 = Person("Kakashi", 34)
 # ORDINARY STATE
 print(person_obj.name)  # Deen
 
-#ORDINARY METHOD 
+# ORDINARY METHOD
 person_obj.introduce()  # Deen says peace upon you!
 person2.say_age()  # John is 25 years old.
 
 
 print(" ===== Ordinary vs static properties")
 
-#STATIC PROPERTY
+# STATIC PROPERTY
 print("Message from the Person class", Person.message)
 # Message from the Person class this is class state property
 
-#STATIC METHOD 
+# STATIC METHOD
 Person.explain()
 
 
+print("==== MAGIC METHODS ====")
+
+# Python's most common magic methods:
+# __init__ , __new__ , __str__ , __call__ , __getitem__ , __leng__ , etc
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *arg):  #  💎*arg is a must. WHY cause we are using more than one arguments in __init__ magic method.
+        print("* __new__*")
+        return super().__new__(cls)
+# NOTE:📝 KEEP IN MIND: before __init__ magic method, starts __new method always runs in the back even though it is not written.
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_engine(self):
+        print(f"{self.name} started the engine!")
+
+    def stop_engine(self):
+        print(f"{self.name} stopped the engine!")
+
+    def __str__(self):
+        return f"The car.name: {self.name} was produced in {self.year}."
+    
+    def __call__(self):  # to make the object function alike
+        print("Object is called as function!")
+        return True 
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+print("-----------")
+your_car = Car("Toyota", 2026)
+#   * __new__*
+print(your_car)  # The car.name: Toyota was produced in 2026.
+response = your_car() # Object is called as function!
+print("response:" , response)  # response: True
