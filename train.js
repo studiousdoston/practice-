ba391b27327436c77a2779953800a1423efb9345
DEEN <studiousdoston@gmail.com>
2026-04-20 16:43:52 +0900

    fix: task formats revised

--- train.js
+++ train.js
@@ -1,56 +1,38 @@
 /*A-TASK (NodeJS)
-
 ⭐️Savol:Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-MASALAN countLetter("e", "engineer") 3ni return qiladi. */
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+ */
 
 
-//⭐️SOLUTION
+// ⭐️ Masalaning yechimi
 function countLetters(char, str) {
   const splitted = str.split(""); //array
   const filtered = splitted.filter(ele => ele === char);
   return filtered.length;
 }
-
-//⭐️RESULT 
 console.log(countLetters("e", "engineering"));
 
+//====================================================================================
 
+/*B-TASK (NodeJS)
+⭐️ SAVOL: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+*/
 
-//*B-TASK (NodeJS)
-
-//⭐️SAVOL: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
-// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
-
-//⭐️SOLUTION
+// ⭐️ Masalaning yechimi
 function countNums(str) {
   return str.split("").map(Number).filter(Boolean).length;
 }
-
-//⭐️RESULT
 console.log(countNums("ad2a54y79wet0sfgb9"));
 
+//====================================================================================
 
+/*CHALLANGE TASK: Animals 
+⭐️ SAVOL shunday bir funksiya tuzingki unga berilgan argumentdagi harflarni hayvonlar arrayida hayvonlarni harflari bilan solishtirsin va mos kelganlaridan yangi bir array hosil qisin
+*/
 
-// CHALLANGE TASK: Animals 
-//⭐️ SAVOL shunday bir funksiya tuzingki unga berilgan argumentdagi harflarni hayvonlar arrayida hayvonlarni harflari bilan solishtirsin va mos kelganlaridan yangi bir array hosil qisin
-
-//⭐️SOLUTION:
+//⭐️ Masalaning yechimi
 const animals = ["fox", "ant", "bird", "lion", "wolf", "deer", "bear", "frog", "hen", "mole", "duck", "goat", "dog", "cat", "bat", "cow"];
-// STEP 1: seperate the individual array elements into arrays consists of words inside the element --> [ 'w', 'o', 'l', 'f' ],
-const animal = animals.map(ele => ele.split("")); //✅
-
-// STEP 2: seperate the argument's individual letters into one array: "fdgwoalt" --> [ 'f', 'd', 'g', 'w', 'o', 'a','l', 't' ]
-const letter = "fdgwoalt" //✅       // should give us ["goat", "wolf", "dog"]
-const letters = letter.split("");
-
-// STEP 3: compare  the two arrays individual elements, and take the elements which exist in both arrays 
-let array = [];
-for (let i = 0; i < animal.length; i++) {
-  if (animal[i].every(letter => letters.includes(letter))) { array.push(animal[i].join("")) }
-}
-//console.log(array)
-
-// STEP 4: create the function
 function findAnimals(word) {
   const animal = animals.map(ele => ele.split(""));
   const letters = word.split("");
@@ -61,8 +43,6 @@ function findAnimals(word) {
     }
   }
   return arrayOfAnimals;
-};   //✅
+};  
 const foundAnimals = findAnimals("fdgwoalt");
-
-//⭐️ RESULT:
 console.log(foundAnimals)
