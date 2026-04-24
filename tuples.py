d6fe51d8c139180a5bb404caaa626c0a34982b2c
DEEN <studiousdoston@gmail.com>
2026-04-24 14:49:27 +0900

    feat: unpacking arguments via tuples

--- tuples.py
+++ tuples.py
@@ -28,3 +28,30 @@ animal = ('dog', 'deer', 'cat', 'fish')
 tuple_obj = ('MIT', 100, True, None)
 print(animal[0])
 # animal[0] = 'falcon'  # TypeError: 'tuple' object does not support item assignment
+
+
+print("====== UNPACKING ARGUMENTS ======")
+groups = ['MIT', 'FLEXY', 'DEVEX', 'MG']
+(x, y, *z) = groups
+print(x)
+print(y)
+print('Rest:', z)
+print('---------------------------')
+
+# *args > tuple
+
+
+def calculate(*args):
+    print('*args:', args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f'the type(args) value: {type(args)}')
+    print('total', total)
+    return
+
+
+# Call
+calculate(1, 7, 9, 2)
+print('---------------------------')
+calculate(1, 4, 356, 2, 8, 7,35)
