eb0f803006d64ebdd510fab6249ba8fc8a4323b0
DEEN <studiousdoston@gmail.com>
2026-04-27 14:38:08 +0900

    feat: learn map filter function

--- lists.py
+++ lists.py
@@ -85,16 +85,18 @@ print(f'the sorted numbers:{numbers} and new_numbers {new_numbers}')
 print("====== Lambda functions ======")
 # lambda is small anonymous function!
 
+
 def calculate(x, y): return x*y
+
 
 result = calculate(3, 5)
 print(result)
 
 people = [
-    ('Robert',20),
+    ('Robert', 20),
     ('Steve', 19),
-    ('Joseph',25),
-    ('Michael',30),
+    ('Joseph', 25),
+    ('Michael', 30),
     ('Ali', 40)
 ]
 people.sort()
@@ -103,3 +105,48 @@ print('people', people)
 # sort by age using lambda
 people.sort(key=lambda person: person[1])
 print('sorted by age:', people)
+
+
+print("====== Enumerate(), Map(), Filter() ======")
+# enumerate for index & value
+
+animals = ['dog', 'cat', 'fish']
+for ele in enumerate(animals):
+    print('element:', ele)
+print('------------------')
+for (i, value) in enumerate(animals):
+    print(f'the index: {i} and value: {value}')
+
+car_obj = dict(brand='Ferrari', year='2025')
+result = car_obj.items()
+print('result:', result)
+
+for (key, value) in result:
+    print(f'the key {key} and value: {value}')
+
+print('---------------------')
+cars = [
+    ('Ferrari', 78),
+    ('Toyota', 87),
+    ('Audi', 116),
+    ('BMW', 109),
+    ('Pagani', 33)
+]
+
+new_cars = []
+for car in cars:
+    new_cars.append(car[0])
+print('new cars:', new_cars)
+
+result1 = map(lambda car: car[0], cars)
+print(f'the result1: {result1} and type: {type(result1)}')
+
+new_cars2 = list(result1)
+print('new cars2:', new_cars2)
+
+print('-------------------')
+# filter
+
+result_filter = filter(lambda car: car[1] > 80, cars)
+print(f'the filtered result: {result_filter} and type: {type(result_filter)}')
+print(list(result_filter))
