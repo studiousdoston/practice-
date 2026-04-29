bdf5d6ce7e4c52fe6484f0b39ee615487325ad26
DEEN <studiousdoston@gmail.com>
2026-04-29 09:24:17 +0900

    feat: How to open txt files

--- debugging.py
+++ debugging.py
@@ -3,15 +3,30 @@
     2. Package Manager & External Package
     3. Debugging
 '''
+import turtle
 print("===== Python Packages & Core Package ======")
 ''' Python Package/Modules: Core🐍, File📒 and External➡️ '''
 
-import turtle
 
-# Core Package
-t = turtle.Turtle()
-t.shape("turtle")
-t.speed(1)
-t.circle(100)
+# # Core Package
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.speed(1)
+# t.circle(100)
 
-turtle.done()
+# turtle.done()
+
+print("----------")
+my_file = open("materials/message.txt", "r")
+try:
+  context = my_file.read()
+  print("content:", context)
+finally:
+  my_file.close()
+
+# with - Context Manager
+with open("materials/message.txt", "r") as your_file:
+  your_content  = your_file.read()
+  print("your_content:", your_content)
+
+print("DONE")
