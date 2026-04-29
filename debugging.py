016cfa4bf24d5a4a94a1307c862f555f08fad679
DEEN <studiousdoston@gmail.com>
2026-04-29 09:10:41 +0900

    feat: Core Package & Turtle example

---
+++ debugging.py
@@ -0,0 +1,17 @@
+''' Packages & Debugging
+    1. Python Packages & Core Package
+    2. Package Manager & External Package
+    3. Debugging
+'''
+print("===== Python Packages & Core Package ======")
+''' Python Package/Modules: Core🐍, File📒 and External➡️ '''
+
+import turtle
+
+# Core Package
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(1)
+t.circle(100)
+
+turtle.done()
