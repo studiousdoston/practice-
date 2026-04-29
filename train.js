19478dbca0cef6594cac35b2c5d5138bed31e3c2
DEEN <studiousdoston@gmail.com>
2026-04-29 12:50:41 +0900

    feat: F - TASK completed

--- train.js
+++ train.js
@@ -1,22 +1,36 @@
+/*F - TASK(NodeJS)
+Shunday findDoublers function tuzing, unga faqat bitta string argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
+  MASALAN: getReverse("hello") return true return qiladi
+*/
+// ⭐️ Masalaning yechimi
+function findDoublers(str) {
+  return str.split("").some(letter => str.split("").filter(ele => ele === letter).length > 1)
+}
+console.log(findDoublers("lovely"))
+
+//====================================================================================
+
 /*E-TASK (NodeJS)
 
 Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
 MASALAN: getReverse("hello") return qilsin "olleh"
  */
-// Masalaning yechimi
+/*⭐️ Masalaning yechimi
 function getReversed(string){
   return string.split("").reverse().join("")
 }
 console.log(getReversed('lovely'))
+*/
 
+//====================================================================================
 
 /*D - TASK(NodeJS)
 
 ⭐️ SAVOL: Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
   MASALAN: getHighestIndex([5, 21, 12, 44, 8]) return qiladi 1 sonini.
-
-⭐️ Masalaning yechimi */
- /*function getHighestIndex(array){
+*/
+/*⭐️ Masalaning yechimi
+function getHighestIndex(array){
    const highestVal =  Math.max(...array);
    return array.indexOf(highestVal)
  }
@@ -24,74 +38,68 @@ console.log(getHighestIndex([5, 21, 12, 44, 8]))
 */
 //====================================================================================
 
-// /*C-TASK (NodeJS)
-// ⭐️ SAVOL: Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
-// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
-//  */
-
-// // ⭐️ Masalaning yechimi
-//  function checkContent(str1, str2){
-//    if (str1.split('').every(ele => str2.split('').includes(ele)) && str2.split('').every(ele => str1.split('').includes(ele))){
-//     return true
-//    }
-//    else 
-//     return false
-//  }
- 
-// console.log(checkContent("mitgroup", "gmtiprou"))
-
-
+/*C-TASK (NodeJS)
+⭐️ SAVOL: Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+*/
+/* ⭐️ Masalaning yechimi
+ function checkContent(str1, str2){
+   if (str1.split('').every(ele => str2.split('').includes(ele)) && str2.split('').every(ele => str1.split('').includes(ele))){
+    return true
+   }
+   else
+    return false
+ }
+console.log(checkContent("mitgroup", "gmtiprou"))
+*/
 
 //====================================================================================
 
-// /*B-TASK (NodeJS)
-// ⭐️ SAVOL: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
-// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
-// */
-
-// // ⭐️ Masalaning yechimi
-// function countNums(str) {
-//   return str.split("").map(Number).filter(Boolean).length;
-// }
-// console.log(countNums("ad2a54y79wet0sfgb9"));
-
-// //====================================================================================
-
-// /*CHALLANGE TASK: Animals 
-// ⭐️ SAVOL shunday bir funksiya tuzingki unga berilgan argumentdagi harflarni hayvonlar arrayida hayvonlarni harflari bilan solishtirsin va mos kelganlaridan yangi bir array hosil qisin
-// */
-
-// //⭐️ Masalaning yechimi
-// const animals = ["fox", "ant", "bird", "lion", "wolf", "deer", "bear", "frog", "hen", "mole", "duck", "goat", "dog", "cat", "bat", "cow"];
-// function findAnimals(word) {
-//   const animal = animals.map(ele => ele.split(""));
-//   const letters = word.split("");
-//   let arrayOfAnimals = [];
-//   for (let i = 0; i < animal.length; i++) {
-//     if (animal[i].every(letter => word.includes(letter))) {
-//       arrayOfAnimals.push(animal[i].join(""))
-//     }
-//   }
-//   return arrayOfAnimals;
-// };
-// const foundAnimals = findAnimals("fdgwoalt");
-// console.log(foundAnimals)
+/*B-TASK (NodeJS)
+⭐️ SAVOL: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+*/
+/* ⭐️ Masalaning yechimi
+function countNums(str) {
+  return str.split("").map(Number).filter(Boolean).length;
+}
+console.log(countNums("ad2a54y79wet0sfgb9"));
+*/
 
 //====================================================================================
 
-// /*A-TASK (NodeJS)
-// ⭐️Savol:Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-// MASALAN countLetter("e", "engineer") 3ni return qiladi.
-//  */
+/*CHALLANGE TASK: Animals
+⭐️ SAVOL shunday bir funksiya tuzingki unga berilgan argumentdagi harflarni hayvonlar arrayida hayvonlarni harflari bilan solishtirsin va mos kelganlaridan yangi bir array hosil qisin
+*/
+/*⭐️ Masalaning yechimi
+const animals = ["fox", "ant", "bird", "lion", "wolf", "deer", "bear", "frog", "hen", "mole", "duck", "goat", "dog", "cat", "bat", "cow"];
+function findAnimals(word) {
+  const animal = animals.map(ele => ele.split(""));
+  const letters = word.split("");
+  let arrayOfAnimals = [];
+  for (let i = 0; i < animal.length; i++) {
+    if (animal[i].every(letter => word.includes(letter))) {
+      arrayOfAnimals.push(animal[i].join(""))
+    }
+  }
+  return arrayOfAnimals;
+};
+const foundAnimals = findAnimals("fdgwoalt");
+console.log(foundAnimals)
+*/
 
+//====================================================================================
 
-// // ⭐️ Masalaning yechimi
-// function countLetters(char, str) {
-//   const splitted = str.split(""); //array
-//   const filtered = splitted.filter(ele => ele === char);
-//   return filtered.length;
-// }
-// console.log(countLetters("e", "engineering"));
-
-
+/*A-TASK (NodeJS)
+⭐️Savol:Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+ */
+/* ⭐️ Masalaning yechimi
+function countLetters(char, str) {
+  const splitted = str.split(""); //array
+  const filtered = splitted.filter(ele => ele === char);
+  return filtered.length;
+}
+console.log(countLetters("e", "engineering"));
+*/
 
