71963322d9f185076919c19902d811a62322b417
DEEN <studiousdoston@gmail.com>
2026-05-11 17:46:05 +0900

    feat: K-TASK completed

--- train.py
+++ train.py
@@ -1,3 +1,26 @@
+'''K-TASK (PYTHON)
+
+Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
+MASALAN: find_longest("I come from Uzbekistan") return "Uzbekistan"'''
+
+# ⭐️ Masalaning yechimi
+def find_longest(text):
+  words = text.split()
+  longest = ''
+  for word in words:
+    if len(word)>len(longest):
+      longest = word
+  return longest
+
+print(find_longest("I come from Uzbekistan"))
+
+  
+'''Steps:
+1.split the string into each words
+2.compare the length of the word with the previous one
+3. return the longest
+'''
+
 '''I-TASK (PYTHON)
 
 Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
